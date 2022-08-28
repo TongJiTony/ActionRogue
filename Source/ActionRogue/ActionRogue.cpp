8df2b8f659c186c6811d0db0fafbbe6bd267e87e
黄子迅 <101124360+TongJiTony@users.noreply.github.com>
2022-08-28 19:57:50 +0800

    Added SCharacter
    -Added Character class
    -Added components

---
+++ Source/ActionRogue/ActionRogue.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActionRogue.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionRogue, "ActionRogue" );
---
+++ Source/ActionRogue/ActionRogueGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRogueGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUE_API AActionRogueGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
